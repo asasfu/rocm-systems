78655a2b7759e76608f83164e3674df4c03cd810
Mukul Joshi <mukul.joshi@amd.com>
2025-01-22 22:36:45 -0500

    kfdtest: Update LoopIsa shader for GFX 12
    Update LoopIsa shader to use s_wait_idle instruction
    on GFX12 as s_waitcnt is not supported on GFX 12.0.3.
    
    Fixes: 3ef406d4 ("kfdtest: update shader store gfx12 and gfx1250")
    Signed-off-by: Mukul Joshi <mukul.joshi@amd.com>
    Change-Id: I337eb77380f58b83d45651bd6700f8d7cd36840d

--- projects/rocr-runtime/libhsakmt/tests/kfdtest/src/ShaderStore.cpp
+++ projects/rocr-runtime/libhsakmt/tests/kfdtest/src/ShaderStore.cpp
@@ -727,12 +727,11 @@ const char *LoopIsa =
         s_cbranch_scc1  END_OF_PGM
         v_add_f32     v0, 2.0, v0
         v_cvt_f32_i32 v17, s1
-        .if (.amdgcn.gfx_generation_number >= 12)
-            s_wait_dscnt     0
-            s_wait_kmcnt     0
-        .else
-            s_waitcnt lgkmcnt(0)
-        .endif
+	.if (.amdgcn.gfx_generation_number >= 12)
+	    s_wait_idle
+	.else
+        s_waitcnt     lgkmcnt(0)
+	.endif
         v_add_f32     v18, s8, v17
         v_add_f32     v19, s9, v17
         v_add_f32     v20, s10, v17
