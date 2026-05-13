c6b20fdec542d61576a2cb1a7bea86f4fd2dcc57
Kandula, Venkateshwar reddy <Venkateshwarreddy.Kandula@amd.com>
2026-05-13 10:32:07 -0500

    [rocprofiler-sdk] remove duplicate --selected-regions-ref-count from rocprofv3.py (#955)
    remove duplicate --selected-regions-ref-count from rocprofv3.py

--- projects/rocprofiler-sdk/source/bin/rocprofv3.py
+++ projects/rocprofiler-sdk/source/bin/rocprofv3.py
@@ -757,11 +757,6 @@ For attachment profiling of running processes:
         "--selected-regions-ref-count",
         help="If set, rocprofv3 will reference count roctxProfilerResume and roctxProfilerPause calls to ignore nested pause/resume pairs",
     )
-    add_parser_bool_argument(
-        filter_options,
-        "--selected-regions-ref-count",
-        help="If set, rocprofv3 will reference count roctxProfilerResume and roctxProfilerPause calls to ignore nested pause/resume pairs",
-    )
 
     perfetto_options = parser.add_argument_group("Perfetto-specific options")
 
