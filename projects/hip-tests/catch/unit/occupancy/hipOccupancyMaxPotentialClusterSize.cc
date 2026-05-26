asasfu/rocm-systems

commit 25dfaa8dd7b0f9244f12bf0685d1598f2b3f8b7c

SWDEV-515087 - Compile away code that uses __cluster_dims__ with a new macro CLUSTER_SUPPORT (#716)
Compile away code that uses __cluster_dims__ with a new macro CLUSTER_SUPPORT; to support --offload-arch strings that mix architectures which support and don't support clusters

projects/hip-tests/catch/include/hip_test_common.hh
@@ -246,6 +246,16 @@ static void initHipCtx(hipCtx_t* pcontext) {
 #define HIP_ARRAY hipArray_t
 #endif
 
+#if defined(__gfx1250__) || defined(__gfx1251__) || defined(__gfx1260__) || defined (__gfx13__)
+// Everytime we use __cluster_dims__ in the tests, we need to wrap the symbol definition with an
+// ifdef checking this macro. It is defined when there is cluster compiler support for the
+// target. When not defined, the compiler would give an error if the attribute is used.
+// If we mix architectures in the offload-arch argument that have support with some that don't,
+// the test would not be compiled away, so checking this macro is always needed when using
+// __cluster_dims__
+#define CLUSTER_SUPPORT 1
+#endif
+
 static inline int getWarpSize() {
 #if HT_NVIDIA
   return 32;

projects/hip-tests/catch/unit/cluster/cluster_class.cc
@@ -22,6 +22,7 @@ THE SOFTWARE.
 #include <hip/hip_cooperative_groups.h>
 
 #include <array>
+#ifdef CLUSTER_SUPPORT
 
 struct cluster_output {
   dim3 block_index;
@@ -96,3 +97,4 @@ TEST_CASE("Unit_cluster_coop_group_class") {
     }
   }
 }
+#endif

projects/hip-tests/catch/unit/cluster/hipClusterCompilerDirective.cc
@@ -22,6 +22,7 @@ THE SOFTWARE.
 #include <hip/hip_ext.h>
 
 #include "ClusterHelper.hpp"
+#ifdef CLUSTER_SUPPORT
 
 /**
  * @addtogroup cluster
@@ -122,3 +123,4 @@ TEST_CASE("Unit_hipClusterLaunch_CompilerDirective_Basic") {
  * End doxygen group ClusterTest.
  * @}
  */
+#endif

projects/hip-tests/catch/unit/occupancy/hipOccupancyMaxActiveClusters.cc
@@ -21,6 +21,7 @@ Testcase Scenarios :
 
 */
 #include "occupancy_common.hh"
+#ifdef CLUSTER_SUPPORT
 
 static __global__ void f1(float* a) { *a = 1.0; }
 static __global__ __cluster_dims__(1, 1, 1) void f1_with_attr(float* a) { *a = 1.0; }
@@ -208,3 +209,4 @@ TEST_CASE("Unit_hipOccupancyMaxActiveClusters_Negative_Parameters") {
     REQUIRE(numClusters == 0);
   }
 }
+#endif

projects/hip-tests/catch/unit/occupancy/hipOccupancyMaxPotentialClusterSize.cc
@@ -23,6 +23,7 @@ Testcase Scenarios :
 #include "occupancy_common.hh"
 #include "distributed_shared_memory.hh"
 #include "resource_guards.hh"
+#ifdef CLUSTER_SUPPORT
 
 static __global__ void f1(float* a) { *a = 1.0; }
 static void host_f1(float* a) { *a = 1.0; }
@@ -176,3 +177,4 @@ TEST_CASE("Unit_hipOccupancyMaxPotentialClusterSize_Negative_Parameters") {
     REQUIRE(clusterSize == 0);
   }
 }
+#endif

commit 57716078577810cda0e035cba29e87bf9cb56cb2

ROCM-9084 - fix cluster occupancy tests compilation and results (#1001)
- Fixes problem introduced in #716 that caused tests that used __cluster_dims__ to be compiled away, even when compiling for just gfx1250, because their associated host code was compiled away too.

Fixes for:

- Unit_hipOccupancyMaxActiveClusters_Positive_RangeValidation: expecting a value that is smaller than it should
- Unit_hipOccupancyMaxPotentialClusterSize_Positive_RangeValidation: temporary workaround for the fact that HSA_AMD_AGENT_INFO_CLUSTER_MAX_SIZE is returning a value that is too high causing the cluster occupancy API to return an unexpected high value causing the test to fail

projects/clr/hipamd/src/hip_module.cpp
@@ -1329,6 +1329,13 @@ hipError_t hipLaunchKernelExC(const hipLaunchConfig_t* config, const void* fPtr,
     HIP_RETURN (hipErrorInvalidConfiguration);
   }
 
+  // Check cluster size against device maximum
+  const auto& deviceInfo = hip::getCurrentDevice()->devices()[0]->info();
+  if (deviceInfo.clusterMaxSize_ > 0 &&
+      clusterDims.x * clusterDims.y * clusterDims.z > deviceInfo.clusterMaxSize_) {
+    HIP_RETURN(hipErrorInvalidClusterSize);
+  }
+
   HIP_RETURN_DURATION(hipLaunchKernel_common(fPtr, config->gridDim, config->blockDim, args,
     config->dynamicSmemBytes, config->stream,
     clusterDims, launchFlags));

projects/clr/hipamd/src/hip_platform.cpp
@@ -861,7 +861,10 @@ hipError_t hipOccupancyMaxPotentialClusterSize(int* clusterSize, const void* f,
   const amd::Device& device = *hip::getCurrentDevice()->devices()[0];
   hipFunction_t func;
   hipError_t hip_error = PlatformState::Instance().StatCO().GetFunc(&func, f, ihipGetDevice());
+  int alu_limited_threads;
+  dim3 clusterDim;
   const amd::device::Info& deviceInfo = device.info();
+  int totalClusterSize;
 
   *clusterSize = 0;
 
@@ -890,7 +893,7 @@ hipError_t hipOccupancyMaxPotentialClusterSize(int* clusterSize, const void* f,
     HIP_RETURN(hipErrorInvalidValue);
   }
 
-  // 1 per WGP (i.e. for a total number equal to half the number of CUs per Shader Engine)
+  // 1 per CU (the result is the number CUs on the smallest Shader Engine of the design)
   // Note that for devices not supporting clustered launches, clusterSize would be set
   // to zero (but the function does not necessarily return an error)
   *clusterSize = device.info().clusterMaxSize_;
@@ -959,7 +962,6 @@ hipError_t hipOccupancyMaxActiveClusters(int* numClusters, const void* f,
   hipFunction_t func;
   hipError_t hip_error = PlatformState::Instance().StatCO().GetFunc(&func, f, ihipGetDevice());
   const amd::device::Info& deviceInfo = device.info();
-
   if ((hip_error != hipSuccess) || (func == nullptr)) {
     HIP_RETURN(hipErrorInvalidDeviceFunction);
   }
@@ -1009,7 +1011,8 @@ hipError_t hipOccupancyMaxActiveClusters(int* numClusters, const void* f,
   if (hip_error == hipSuccess) {
     // a maximum of 15 total clusters in flight per shader engine are possible (gfx1250)
     static constexpr int MaxClustersPerSE = 15;
-    int clustersPerSE = (numBlocks * deviceInfo.clusterMaxSize_) / totalClusterSize;
+    int computeUnitsPerSE = deviceInfo.maxComputeUnits_ / deviceInfo.numberOfShaderEngines_;
+    int clustersPerSE = (numBlocks * computeUnitsPerSE) / totalClusterSize;
 
     clustersPerSE = std::min(clustersPerSE, MaxClustersPerSE);
     *numClusters = clustersPerSE * deviceInfo.numberOfShaderEngines_;

projects/clr/rocclr/device/rocm/rocdevice.cpp
@@ -1747,8 +1747,13 @@ bool Device::populateOCLDeviceConstants() {
 
   // this is required for clustered kernel launches; but it might not be supported in older rocr,
   // so invalid argument might no be necessarily an error
-  if (HSA_STATUS_SUCCESS != hsaStatus && HSA_STATUS_ERROR_INVALID_ARGUMENT != hsaStatus)
+  if (HSA_STATUS_SUCCESS != hsaStatus && HSA_STATUS_ERROR_INVALID_ARGUMENT != hsaStatus) {
     LogError("HSA_AMD_AGENT_INFO_CLUSTER_MAX_SIZE query failed");
+  } else {
+    // this is a temporary override of the ROCr result. This line will be removed as soon as ROCr
+    // provides the correct result
+    info_.clusterMaxSize_ = info_.maxComputeUnits_ / info_.numberOfShaderEngines_;
+  }
 
   info_.gpuDirectRdmaWithHipVmmSupported_ =
       info_.virtualMemoryManagement_ && info_.dmabufSupported_;

projects/hip-tests/catch/config/configs/unit/cluster.yaml
@@ -2,10 +2,10 @@
 cluster:
   Unit_cluster_coop_group_class:
     <<: *level_2
-    disabled: [gfx1260, gfx1310, gfx1370]
+    disabled: [gfx1310, gfx1370]
   Unit_hipClusterLaunch_CompilerDirective_Basic:
     <<: *level_2
-    disabled: [gfx1260, gfx1310, gfx1370]
+    disabled: [gfx1310, gfx1370]
   Unit_hipClusterLaunch_LaunchApi_Basic:
     <<: *level_2
-    disabled: [gfx1260, gfx1310, gfx1370]
+    disabled: [gfx1310, gfx1370]

projects/hip-tests/catch/config/configs/unit/occupancy.yaml
@@ -89,19 +89,19 @@ occupancy:
     disabled: [gfx1310, gfx1370]
   Unit_hipOccupancyMaxActiveClusters_Negative_Parameters:
     <<: *level_2
-    disabled: [gfx1260, gfx1310, gfx1370]
+    disabled: [gfx1310, gfx1370]
   Unit_hipOccupancyMaxActiveClusters_Negative_Zero_Cluster:
     <<: *level_2
-    disabled: [gfx1260, gfx1310, gfx1370]
+    disabled: [gfx1310, gfx1370]
   Unit_hipOccupancyMaxActiveClusters_Positive_RangeValidation:
     <<: *level_2
     disabled: [gfx1260, gfx1310, gfx1370]
   Unit_hipOccupancyMaxPotentialClusterSize_Negative_Parameters:
     <<: *level_2
-    disabled: [gfx1260, gfx1310, gfx1370]
+    disabled: [gfx1310, gfx1370]
   Unit_hipOccupancyMaxPotentialClusterSize_Positive_RangeValidation:
     <<: *level_2
-    disabled: [gfx1260, gfx1310, gfx1370]
+    disabled: [gfx1310, gfx1370]
   Unit_hipOccupancyMaxPotentialClusterSize_Verify_Launch: *level_2
   Unit_hipOccupancyMaxActiveBlocksPerMultiprocessor_rangeValidation:
     <<: *level_2

projects/hip-tests/catch/include/hip_test_common.hh
@@ -245,12 +245,14 @@ static void initHipCtx(hipCtx_t* pcontext) {
 
 #if defined(__gfx1250__) || defined(__gfx1251__) || defined(__gfx1260__) || defined (__gfx13__)
 // Everytime we use __cluster_dims__ in the tests, we need to wrap the symbol definition with an
-// ifdef checking this macro. It is defined when there is cluster compiler support for the
+// ifdef checking support. That attributed is defined when there is cluster compiler support for the
 // target. When not defined, the compiler would give an error if the attribute is used.
 // If we mix architectures in the offload-arch argument that have support with some that don't,
-// the test would not be compiled away, so checking this macro is always needed when using
+// the test would not be compiled away, so using these macros is always needed when using
 // __cluster_dims__
-#define CLUSTER_SUPPORT 1
+#define CLUSTER_DIMS(...) __cluster_dims__(__VA_ARGS__)
+#else
+#define CLUSTER_DIMS(...)
 #endif
 
 static inline int getWarpSize() {

projects/hip-tests/catch/unit/cluster/CMakeLists.txt
@@ -29,7 +29,7 @@ set(TEST_SRC
 )
 
 # Append it to the list
-set(CLUSTER_SUPPORTING_GPUS "gfx1250")
+set(CLUSTER_SUPPORTING_GPUS "gfx1250" "gfx1260")
 set(CLUSTER_SUPPORTED OFF)
 
 foreach(CLUSTER_SUPPORTING_GPU IN LISTS CLUSTER_SUPPORTING_GPUS)

projects/hip-tests/catch/unit/cluster/cluster_class.cc
@@ -22,7 +22,6 @@ THE SOFTWARE.
 #include <hip/hip_cooperative_groups.h>
 
 #include <array>
-#ifdef CLUSTER_SUPPORT
 
 struct cluster_output {
   dim3 block_index;
@@ -35,7 +34,7 @@ struct cluster_output {
   unsigned int num_threads;
 };
 
-static __global__ void __cluster_dims__(2, 1, 1) cluster_class_validation(cluster_output* out) {
+static __global__ void CLUSTER_DIMS(2, 1, 1) cluster_class_validation(cluster_output* out) {
   namespace cg = cooperative_groups;
   cg::cluster_group c = cg::this_cluster();
   size_t i = blockIdx.x * blockDim.x + threadIdx.x;
@@ -97,4 +96,3 @@ HIP_TEST_CASE(Unit_cluster_coop_group_class) {
     }
   }
 }
-#endif

projects/hip-tests/catch/unit/cluster/hipClusterCompilerDirective.cc
@@ -22,7 +22,6 @@ THE SOFTWARE.
 #include <hip/hip_ext.h>
 
 #include "ClusterHelper.hpp"
-#ifdef CLUSTER_SUPPORT
 
 /**
  * @addtogroup cluster
@@ -35,8 +34,7 @@ THE SOFTWARE.
 #define CLY 1
 #define CLZ 1
 
-__global__ void __cluster_dims__(CLX, CLY, CLZ)
-    ClusterLaunchKernelBasicCD(int* in, int* out, int n) {
+__global__ void CLUSTER_DIMS(CLX, CLY, CLZ) ClusterLaunchKernelBasicCD(int* in, int* out, int n) {
   int idx = blockDim.x * blockIdx.x + threadIdx.x;
   if (idx < n) {
     out[idx] = in[idx];
@@ -123,4 +121,3 @@ HIP_TEST_CASE(Unit_hipClusterLaunch_CompilerDirective_Basic) {
  * End doxygen group ClusterTest.
  * @}
  */
-#endif

projects/hip-tests/catch/unit/occupancy/CMakeLists.txt
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: MIT
 
 # Common Tests - Test independent of all platforms
-set(TARGETS_SUPPORTING_CLUSTERS gfx1250 gfx1251)
+set(TARGETS_SUPPORTING_CLUSTERS gfx1250 gfx1251 gfx1260)
 set(CLUSTERS_SUPPORTED -1)
 
 function(CheckRejectedArchs OFFLOAD_ARCH_STR_LOCAL)

projects/hip-tests/catch/unit/occupancy/hipOccupancyMaxActiveClusters.cc
@@ -21,10 +21,9 @@ Testcase Scenarios :
 
 */
 #include "occupancy_common.hh"
-#ifdef CLUSTER_SUPPORT
 
 static __global__ void f1(float* a) { *a = 1.0; }
-static __global__ __cluster_dims__(1, 1, 1) void f1_with_attr(float* a) { *a = 1.0; }
+static __global__ CLUSTER_DIMS(1, 1, 1) void f1_with_attr(float* a) { *a = 1.0; }
 static void host_f1(float* a) { *a = 1.0; }
 static __global__ void reverse(int* d, int n) {
   __shared__ int shBuf[64];
@@ -44,7 +43,10 @@ HIP_TEST_CASE(Unit_hipOccupancyMaxActiveClusters_Positive_RangeValidation) {
   // maximum number of blocks per CU using no shared memory
   int maxBlocks;
   hipDeviceProp_t props;
-  int numSharedEngines;
+  int numXCDs;
+  int sePerXCD;
+  int totalSE;
+  int maxInFlightClusterPerSPI;
 
   HIP_CHECK(hipGetDeviceProperties(&props, 0));
 
@@ -70,16 +72,19 @@ HIP_TEST_CASE(Unit_hipOccupancyMaxActiveClusters_Positive_RangeValidation) {
   HIP_CHECK(hipOccupancyMaxActiveBlocksPerMultiprocessor(
       &maxBlocks, reinterpret_cast<const void*>(f1), 128, 0));
   REQUIRE(maxBlocks > 0);
-  // if using a partial simulation; this number would need to be changed
-  // (e.g. to 2 SEs if using 2 XCCs)
-  numSharedEngines = props.multiProcessorCount / maxClusterSize;
 
   // these check that shared memory usage has an effect on occupancy calculations
   HIP_CHECK(
       hipOccupancyMaxActiveClusters(&clustersNoLDS, reinterpret_cast<const void*>(f1), &config));
   INFO("occupancy with no shared memory: " << clustersNoLDS);
+
+  numXCDs = 8;
+  sePerXCD = 2;
+  totalSE = numXCDs * sePerXCD;
+  maxInFlightClusterPerSPI = 15;
+
   // limited by the number of in-flight clusters per SE
-  REQUIRE(clustersNoLDS == 30);
+  REQUIRE(clustersNoLDS == totalSE * maxInFlightClusterPerSPI);
 
   config.dynamicSmemBytes = props.sharedMemPerBlock / 8;
   HIP_CHECK(
@@ -88,7 +93,7 @@ HIP_TEST_CASE(Unit_hipOccupancyMaxActiveClusters_Positive_RangeValidation) {
 
   float ratio = maxBlocks / static_cast<float>(clusterDim.x * clusterDim.y * clusterDim.z);
   // limited due to shared memory
-  REQUIRE(clustersLDS == (maxClusterSize * ratio * numSharedEngines) / 2);
+  REQUIRE(clustersLDS == (maxClusterSize * ratio * totalSE) / 2);
 
   config.dynamicSmemBytes = props.sharedMemPerBlock / 4;
   HIP_CHECK(
@@ -96,7 +101,7 @@ HIP_TEST_CASE(Unit_hipOccupancyMaxActiveClusters_Positive_RangeValidation) {
   INFO("occupancy non-max cluster size: " << clustersLDS);
   // further limited by shared memory; the number of clusters per SE is lower than the maximum
   // cluster size
-  REQUIRE(clustersLDS == (maxBlocks * ratio * numSharedEngines) / 4);
+  REQUIRE(clustersLDS == (maxBlocks * ratio * totalSE) / 4);
 }
 
 HIP_TEST_CASE(Unit_hipOccupancyMaxActiveClusters_Negative_Zero_Cluster) {
@@ -209,4 +214,3 @@ HIP_TEST_CASE(Unit_hipOccupancyMaxActiveClusters_Negative_Parameters) {
     REQUIRE(numClusters == 0);
   }
 }
-#endif

projects/hip-tests/catch/unit/occupancy/hipOccupancyMaxPotentialClusterSize.cc
@@ -23,7 +23,6 @@ Testcase Scenarios :
 #include "occupancy_common.hh"
 #include "distributed_shared_memory.hh"
 #include "resource_guards.hh"
-#ifdef CLUSTER_SUPPORT
 
 static __global__ void f1(float* a) { *a = 1.0; }
 static void host_f1(float* a) { *a = 1.0; }
@@ -52,7 +51,7 @@ HIP_TEST_CASE(Unit_hipOccupancyMaxPotentialClusterSize_Positive_RangeValidation)
     INFO("Max potential cluster size is: " << clusterSize);
     // at the time of this writing a SPI could be drive up to 15 CUs. The number of CUs per SE
     // varies per silicon, but any AMD silicon should have at least 8
-    REQUIRE((clusterSize > 8 && clusterSize <= 16));
+    REQUIRE((clusterSize >= 8 && clusterSize <= 16));
   } else {
     REQUIRE(hip_error == hipErrorInvalidClusterSize);
   }
@@ -100,6 +99,15 @@ HIP_TEST_CASE(Unit_hipOccupancyMaxPotentialClusterSize_Verify_Launch) {
   numBytes = clusterSize * config.dynamicSmemBytes;
 
   SECTION("launch with maximum size shmem") {
+    // This section uses distributed shared memory (cluster.map_shared_rank)
+    // which is not yet supported on gfx1260.
+    hipDeviceProp_t devProps;
+    HIP_CHECK(hipGetDeviceProperties(&devProps, 0));
+    if (std::string(devProps.gcnArchName).find("gfx1260") != std::string::npos) {
+      SUCCEED("distributed shared memory not supported on gfx1260");
+      return;
+    }
+
     int expected = 0;
 
     d_output = LinearAllocGuard<int>(LinearAllocs::hipMalloc, numBytes);
@@ -177,4 +185,3 @@ HIP_TEST_CASE(Unit_hipOccupancyMaxPotentialClusterSize_Negative_Parameters) {
     REQUIRE(clusterSize == 0);
   }
 }
-#endif