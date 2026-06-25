b95bcd322d3bbd8e9123c4a351a8b0fcb52c8f17
zbyrne <zbyrne@amd.com>
2026-06-25 14:31:43 -0600

    feat(hipfile): Expose async buffer size through Configuration interface.

--- projects/hipfile/src/amd_detail/configuration.cpp
+++ projects/hipfile/src/amd_detail/configuration.cpp
@@ -64,3 +64,14 @@ Configuration::unsupportedFileSystems() const noexcept
     static bool unsupported_file_systems_env{Environment::unsupported_file_systems().value_or(false)};
     return unsupported_file_systems_env;
 }
+
+size_t
+Configuration::asyncBufferSize() const noexcept
+{
+    static size_t async_buffer_size_env{[] {
+        constexpr size_t default_size{1 << 20}; // 1 MiB
+        auto             env = Environment::async_buffer_size();
+        return (env && *env > 0) ? *env : default_size;
+    }()};
+    return async_buffer_size_env;
+}
