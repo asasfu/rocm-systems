fd37c35b825e156a6f345cddf06e8885022751b5
Freehill, Chris <Chris.Freehill@amd.com>
2026-06-25 18:34:19 -0500

    rocr: Improvements when running rocrtst with FFM (#1164)
    - Removed an assert that isn't valid in all cases
    - Added and use wildcards for rocrtst filtering of tests not
      to run
    - Fixed how some tests use rocrtst filtering
    
    Co-authored-by: Chris Freehill <cfreehil@amd.com>

--- projects/rocr-runtime/libhsakmt/src/queues.c
+++ projects/rocr-runtime/libhsakmt/src/queues.c
@@ -131,9 +131,6 @@ static uint32_t get_hwreg_size_per_cu(const HsaNodeProperties *node, uint32_t gf
 
 	hwreg_size_bytes = num_waves_per_simd * simd_per_cu * bytes_per_wave;
 
-	assert(hwreg_size_bytes == (
-		gfxv == GFX_VERSION_GFX1250 ?	0x8000 :
-										0x1000));
 	return hwreg_size_bytes;
 }
 
