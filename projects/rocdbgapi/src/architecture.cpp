c854353ca48809d37e349aba9d11e01bc6365b8c
Shahab Vahedi <shahab.vahedi@amd.com>
2026-07-01 12:23:00 +0200

    architecture: Add gfx1170, gfx1171, and gfx1172 support
    Add ELF mach values for gfx1170, gfx1171, and gfx1172.
    Also, create their respective architectures and update the doc.

--- projects/rocdbgapi/src/architecture.cpp
+++ projects/rocdbgapi/src/architecture.cpp
@@ -6218,6 +6218,36 @@ public:
   }
 };
 
+class gfx1170_t final : public gfx11_architecture_t
+{
+public:
+  gfx1170_t ()
+    : gfx11_architecture_t (EF_AMDGPU_MACH_AMDGCN_GFX1170,
+                            "amdgcn-amd-amdhsa--gfx1170")
+  {
+  }
+};
+
+class gfx1171_t final : public gfx11_architecture_t
+{
+public:
+  gfx1171_t ()
+    : gfx11_architecture_t (EF_AMDGPU_MACH_AMDGCN_GFX1171,
+                            "amdgcn-amd-amdhsa--gfx1171")
+  {
+  }
+};
+
+class gfx1172_t final : public gfx11_architecture_t
+{
+public:
+  gfx1172_t ()
+    : gfx11_architecture_t (EF_AMDGPU_MACH_AMDGCN_GFX1172,
+                            "amdgcn-amd-amdhsa--gfx1172")
+  {
+  }
+};
+
 class gfx12_architecture_t : public gfx11_architecture_t
 {
 private:
@@ -8540,6 +8570,9 @@ decltype (architecture_t::s_architecture_map)
       map.emplace (make_architecture<gfx1151_t> ());
       map.emplace (make_architecture<gfx1152_t> ());
       map.emplace (make_architecture<gfx1153_t> ());
+      map.emplace (make_architecture<gfx1170_t> ());
+      map.emplace (make_architecture<gfx1171_t> ());
+      map.emplace (make_architecture<gfx1172_t> ());
       map.emplace (make_architecture<gfx12_generic_t> ());
       map.emplace (make_architecture<gfx1200_t> ());
       map.emplace (make_architecture<gfx1201_t> ());
